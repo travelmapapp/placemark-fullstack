daead3ed892ffd76ccb0b4cb15220185d691b38f
clem <clementm.mas@gmail.com>
2024-08-11 22:08:47 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ loadEnvConfig(projectDir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()] as any,
   test: {
     dir: "./",
     deps: {
